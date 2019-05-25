ca073bb4aea2a469a68ef7db68e86056cfc33442
Jason Hilton <jason_hilton@yahoo.com>
2019-05-25 18:36:33 +0100

    tidied results

--- run_model.R
+++ run_model.R
@@ -3,38 +3,65 @@ library(yaml)
 cmd_arg <- commandArgs(trailingOnly = T)
 
 if (length(cmd_arg)==0){
-  cmd_arg <- c("design.csv", 1)
+  ff <- tail(list.files("designs/lhs/"),1)
+  cmd_arg <- c(file.path("designs","lhs",ff), 1, 1)
 }
 
 design_file <- cmd_arg[1]
 run_no <- cmd_arg[2] # ie. csv line
+rep <- cmd_arg[3] # ie. csv line
+if (length(rep)==0){
+  rep <- 0
+}
 
 design_space <- read.csv(design_file)
-
+time_stamp <- stringi::stri_extract(design_file, regex="[0-9]{8}_[0-9]{6}")
 
 point <- design_space[run_no,]
 
-
+## Parameter args --------------------------------------------------------------
 param_names <- paste0("--", gsub("_", "-",names(point)))
 
 param_args <- mapply(function(name, val) paste(name,val), 
                      param_names, as.numeric(point))
 
 arg <- paste(param_args, collapse = " ")
-# should I save these in a output folder that actually makes sense
-output_opts <- c("--log-file log_", "--city-file cities_",
-                 "--link-file link_", "--par-file par_", "--out-file out_")
 
+
+## Output args -----------------------------------------------------------------
+out_dir <- file.path("results", time_stamp)
+
+dir.create(out_dir,
+           recursive = T,
+           showWarnings = F)
+
+out_opts <- c("--log-file", "--city-file",
+          "--link-file", "--par-file", "--out-file")
+out_prefixes <- c("log", "cities",
+              "link", "par", "out")
+
+out_arg <- mapply(paste, 
+                  out_opts,
+                  paste(out_dir, out_prefixes, sep="/"))
+out_arg <- paste0(out_arg, 
+                  "_",
+                  run_no,
+                  "_",
+                  rep,
+                  ".txt")
+
+## Control meta parameters -----------------------------------------------------
 meta_pars <- read_yaml("meta_pars.yaml")
 meta_param_names <- paste0("--", gsub("_", "-",names(meta_pars)))
 
 meta_arg <- mapply(function(name, val) paste(name,val),
        meta_param_names, as.numeric(meta_pars))
 
-
 rand_arg <- paste0("--rand-seed-sim ", sample(10000,1))
 
-arg <- paste(c(arg, paste0(output_opts, run_no, ".txt"),
+## Stick them together ---------------------------------------------------------
+
+arg <- paste(c(arg, out_arg,
                rand_arg, meta_arg), collapse=" ")
 
 
@@ -42,4 +69,8 @@ Sys.setenv(JULIA_LOAD_PATH="/storage/RRGraphs:")
 run_cmd <- paste0("julia ../RRGraphs/run.jl ", arg)
 
 system(run_cmd)
+
+
+
+
 
--- send_jobs.sh
+++ send_jobs.sh
@@ -2,8 +2,17 @@
 
 module load R/3.5.1
 
-
+Rscript gen_lhs_design.R
+csv_file=$(ls design/lhs | tail -n 1)
 runs=$(cat $csv_file | wc --lines)
-arrayid=$(qsub run_model.pbs -v DESIGN_FILE=$csv_file -t 1-$runs)
+runs=$((runs-1)) # one line is the headers!
+arrayid=$(qsub run_model.pbs -v DESIGN_FILE=$csv_file,REP=1 -t 1-$runs)
 
-# qsub -W depend=afterokarray:$arrayid PBS/run_loos.pbs
+# strategy - after the first one, do a loop for the repetitions
+# qsub -W depend=afteranyarray:$arrayid PBS/run_loos.pbs
+
+#echo $one 
+#for id in seq 2 6; do 
+# newarray=$(qsub -W depend=afteranyarray:$arrayid run_model.pbs DESIGN_FILE=$csv_file REP=$id)
+# arrayid=$newarray
+#done
