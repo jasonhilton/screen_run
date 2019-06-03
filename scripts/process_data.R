3ef6a97141070f105c4dec94bbee858c480332d5
J.D.Hilton <jdh1d15@cyan03.(none)>
2019-06-03 21:23:03 +0100

    bug fixes from iridis

--- scripts/process_data.R
+++ scripts/process_data.R
@@ -26,8 +26,9 @@ res_path <- file.path("results", results_date)
 files <- list.files(res_path, pattern = "par_[0-9]{1+}_1.txt")
 
 par_df <- map_df(files, load_params, res_path=res_path)
+dir.create(file.path("results", "summary", results_date), recursive=T)
 
-saveRDS(par_Df, file.path( "results", "summary", results_date, "par.rds"))
+saveRDS(par_df, file.path( "results", "summary", results_date, "par.rds"))
 
 ## logs ------------------------------------------------------------------------
 files <- list.files(res_path, pattern = "log*")
@@ -39,7 +40,6 @@ logs_df %<>% rename(mean_cap = `# mean_cap`)
 
 logs_df %<>% left_join(par_df)
 
-dir.create(file.path("results", "summary", results_date), recursive=T)
 saveRDS(logs_df, file.path("results", "summary",results_date, "log.rds"))
 
 
@@ -47,7 +47,7 @@ saveRDS(logs_df, file.path("results", "summary",results_date, "log.rds"))
 
 files <- list.files(res_path, pattern = "cities*")
 
-city_df <- map_df(files, parse_file, res_path=res_path)
+city_df <- map_df(files, load_file, res_path=res_path)
 
 city_df %<>% filter(type=="EXIT") %>% rename(id=`# id`) %>%
   select(-x, -qual,-N)
