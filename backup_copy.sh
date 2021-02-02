3b1209a8e7cc6ce0285f36393021e87ab64361ce
J.D.Hilton <jdh1d15@cyan03.(none)>
2021-02-02 13:58:06 +0000

    backup location edit

--- backup_copy.sh
+++ backup_copy.sh
@@ -1,5 +1,5 @@
 local_path=$1
-remote_path=/research/CPCModelling/param_fert/$1
+remote_path=/research/CPCModelling/screen_run/$1
 
 ssh jdh1d15@ssh.soton.ac.uk remote_path=$remote_path "mkdir -p $remote_path"
 scp $local_path/* jdh1d15@ssh.soton.ac.uk:$remote_path
