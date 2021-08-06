da4541ebd6e79c3c938f72537d47a60dd2a4a158
Jason Hilton <jason_hilton@yahoo.com>
2021-08-06 21:45:43 +0100

    some changes

--- send_jobs.sh
+++ send_jobs.sh
@@ -3,9 +3,9 @@
 module load conda
 source activate Rstan
 
-Rscript scripts/gen_lhs_design.R
+Rscript scripts/gen_lhs_design.R config/varied_pars_$1.yaml # config varied_pars file (M3 or M4)
 csv_file=$(ls designs/lhs | tail -n 1)
 runs=$(cat designs/lhs/$csv_file | wc --lines)
 runs=$((runs-1)) # one line is the headers!
-arrayid=$(qsub run_model.pbs -v DESIGN_FILE=designs/lhs/$csv_file,N_REPS=12 -t 1-$runs)
+arrayid=$(qsub run_model.pbs -v MOD_VERSION=$1,DESIGN_FILE=designs/lhs/$csv_file,N_REPS=12 -t 1-$runs)
 
